90128e3d6b5f2b4a4a9c4d53660d2711d795fa37
thenoobchad <noobchaddy@gmail.com>
2025-04-16 13:15:29 +0100

    add hero animation

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ export default function App() {
   return (
     <main className="relative min-h-screen w-screen">
       <Hero/>
+
+      <section className="z-0 min-h-screen bg-blue-500"/>
     </main>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,15 @@
-import { useRef } from "react"
+import { useEffect, useRef } from "react"
 import { useState } from "react"
 import Button from "./Button"
 import { TiLocationArrow } from "react-icons/ti"
+import { useGSAP } from "@gsap/react"
+import gsap from 'gsap'
+import { ScrollTrigger } from "gsap/all"
+
+
+
+
+gsap.registerPlugin(ScrollTrigger)
 
 
 export default function Hero() {
@@ -11,7 +19,7 @@ export default function Hero() {
     const [isLoading, setIsLoading] = useState(true)
     const [loadedVideos, setLoadedVideos] = useState(0)
 
-    const totalVideo = 3;
+    const totalVideos = 3;
 
     const nextVideoRef = useRef(null)
 
@@ -19,7 +27,7 @@ export default function Hero() {
         setLoadedVideos((prev) => prev +1)
     }
 
-    const upcomingVideoIndex = (currentIndex % totalVideo) + 1
+    const upcomingVideoIndex = (currentIndex % totalVideos) + 1
 
     const handleMiniVdClick = () => {
         setHasClicked(true)
@@ -27,10 +35,72 @@ export default function Hero() {
         setCurrentIndex(upcomingVideoIndex)
     }
 
+
+useEffect(()=>{
+    if(loadedVideos === totalVideos - 1) {
+        setIsLoading(false)
+    }
+}, [loadedVideos])
+
+
+
+    useGSAP(() => {
+        if(hasClicked){
+            gsap.set("#next-video", {visibility:"visible"});
+
+            gsap.to('#next-video', {
+                transformOrigin:"center center",
+                scale:1,
+                width:'100%',
+                height:'100%',
+                duration: 1,
+                ease:"power1.inOut",
+                onStart:() => nextVideoRef.current.play(),
+            })
+
+            gsap.from('#current-video', {
+                transformOrigin:"center center",
+                scale:0,
+                duration:1.5,
+                ease:"power1.inOut"
+            })
+        }
+    }, { dependencies: [currentIndex], revertOnUpdate:true})
+
+
+    useGSAP(() => {
+        gsap.set("video-frame", {
+            clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
+            borderRadius: '0 0 40% 10%'
+        })
+
+        gsap.from("#video-frame", {
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+            borderRadius: '0 0 0 0',
+            ease: "power1.inOut",
+            scrollTrigger:{
+                trigger:"#video-frame",
+                start:'center center',
+                end:'bottom center',
+                scrub: true,
+            }
+        })
+    })
+
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`
 
   return (
-    <div className="max-h-dvh w-screen overflow-x-hidden">
+    <div className="h-dvh relative w-screen overflow-x-hidden">
+
+        {isLoading && (
+            <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+                <div className="three-body">
+                    <div className="three-body__dot"/>
+                    <div className="three-body__dot"/>
+                    <div className="three-body__dot"/>
+                </div>
+            </div>
+        )}
         <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
             <div>
                 <div className="mask-clip-path absolute absolute-center z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
@@ -58,7 +128,7 @@ export default function Hero() {
                 />
 
                 <video 
-                src={getVideoSrc(currentIndex === totalVideo - 1 ? 1 : currentIndex)}
+                src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
 
                 autoPlay
                 loop
@@ -82,7 +152,7 @@ export default function Hero() {
             </div>
         </div>
 
-        <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">G<b>a</b>ming</h1>
+        <h1 className="special-font hero-heading absolute bottom-5 right-5 z-60 text-black">G<b>a</b>ming</h1>
 
     </div>
   )
