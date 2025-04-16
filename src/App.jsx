b3c9eb5a91b7278c5fa6b28e9df37c62423a0dea
thenoobchad <noobchaddy@gmail.com>
2025-04-16 11:35:39 +0100

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Hero from "./components/Hero";
 
 
 export default function App() {
   return (
-    <main>
-      <h1 className="text-5xl text-orange-500 font-bold">Welcome to Awwwards</h1>
+    <main className="relative min-h-screen w-screen">
+      <Hero/>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,31 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        zentry:['zentry', 'sanf-serif'],
+        general:['general','sanf-serif'],
+        'circular-web':['circular-web','sanf-serif'],
+        'robert-medium':['robert-medium','sanf-serif'],
+        'robert-regular':['robert-regular','sanf-serif'],
+      },
+      colors:{
+        blue:{
+          50:"#dfdff0",
+          75:"#dfdff2",
+          100:"#f0f2fa",
+          200:"#010101",
+          300:"#4fb7dd"
+        },
+        violet:{
+          300:"#5724ff"
+        },
+        yellow:{
+          100:"#8e983f",
+          300:"#edff66"
+        }
+      }
+    },
   },
   plugins: [],
 }
